801c803515aeeb4b44c6189cced52d273037fdc5
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-10-08 00:50:17 +0300

    menu file

--- CS213-2023-20220251-20220404-20220044-A1-Part1.cpp
+++ CS213-2023-20220251-20220404-20220044-A1-Part1.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 /* functions declaration */
 void Ahlan();
-int check_file_name(string file_name);
+bool check_file_name(string file_name);
 
 //================================================================================================================================//
 
@@ -18,13 +18,88 @@ int check_file_name(string file_name);
 int main(void)
 {
 	string file_name;
+	char selection;
 
 	Ahlan();
 	cin >> file_name;
-	int check_FN = check_file_name(file_name);
+	bool check_FN = check_file_name(file_name + ".bmp");
 
-	if (!check_FN)
+	if (check_FN)
 		return (1);
+
+	cout << "Please select a filter to apply or 0 to exit:"
+		 << "\n1-Black & White Filter"
+		 << "\n2-Invert Filter"
+		 << "\n3-Merge Filter"
+		 << "\n4-Flip Image"
+		 << "\n5-Darken and Lighten Image"
+		 << "\n6-Rotate Image"
+		 << "\n7-Detect Image Edges"
+		 << "\n8-Enlarge Image"
+		 << "\n9-Shrink Image"
+		 << "\na-Mirror 1/2 Image"
+		 << "\nb-Shuffle Image"
+		 << "\nc-Blur Image"
+		 << "\nd-Crop Image"
+		 << "\ne-Skew Image Right"
+		 << "\nf-Skew Image Up"
+		 << "\ns-Save the image to a file"
+		 << "\n0-Exit\n";
+
+	switch (selection)
+	{
+	case '1':
+		/* code */
+		break;
+	case '2':
+		/* code */
+		break;
+	case '3':
+		/* code */
+		break;
+	case '4':
+		/* code */
+		break;
+	case '5':
+		/* code */
+		break;
+	case '6':
+		/* code */
+		break;
+	case '7':
+		/* code */
+		break;
+	case '8':
+		/* code */
+		break;
+	case '9':
+		/* code */
+		break;
+	case 'a':
+		/* code */
+		break;
+	case 'b':
+		/* code */
+		break;
+	case 'c':
+		/* code */
+		break;
+	case 'd':
+		/* code */
+		break;
+	case 'e':
+		/* code */
+		break;
+	case 'f':
+		/* code */
+		break;
+	case 's':
+		/* code */
+		break;
+	default:
+		return (0);
+		break;
+	}
 
 	return (0);
 }
@@ -41,22 +116,17 @@ void Ahlan(void)
 /**
  * check_file_name - is a function to check if the given file name is found or not.
  * @file_name: the name of the file
- * Return: 0 on fail and 1 on success.
+ * Return: 1 on fail and 0 on success.
  */
-int check_file_name(string file_name)
+bool check_file_name(string file_name)
 {
-	FILE *file;
+	FILE *file = fopen(file_name.c_str(), "rb");
 
-	char c[file_name.length()];
-	for (int i = 0; i < file_name.length(); i++)
+	if ((file))
 	{
-		c[i] = file_name[i];
-	}
-
-	if (!(file = fopen(strcat(c, ".bmp"), "rb")))
-	{
+		fclose(file);
 		return (0);
 	}
-	else
-		return (1);
+
+	return (1);
 }
