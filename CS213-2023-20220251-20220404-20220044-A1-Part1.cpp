6ef97555913a3020aa720d96898e26b3cd628015
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-10-08 01:39:55 +0300

    main menu are done

--- CS213-2023-20220251-20220404-20220044-A1-Part1.cpp
+++ CS213-2023-20220251-20220404-20220044-A1-Part1.cpp
@@ -1,15 +1,22 @@
 #include <bits/stdc++.h>
 #include "bmplib.cpp"
 
+#include "CS213-2023-20220044-A1.cpp"
+
 using namespace std;
 
 //==================================================================================================================================//
 
 /* functions declaration */
+void loadImage(string file_name);
+void saveImage();
 void Ahlan();
+void show_menu();
 bool check_file_name(string file_name);
 
-//================================================================================================================================//
+unsigned char image[SIZE][SIZE];
+
+//==================================================================================================================================//
 
 /**
  * main - the main block to execute the image_filter proccessing application.
@@ -27,90 +34,126 @@ int main(void)
 	if (check_FN)
 		return (1);
 
-	cout << "Please select a filter to apply or 0 to exit:"
-		 << "\n1-Black & White Filter"
-		 << "\n2-Invert Filter"
-		 << "\n3-Merge Filter"
-		 << "\n4-Flip Image"
-		 << "\n5-Darken and Lighten Image"
-		 << "\n6-Rotate Image"
-		 << "\n7-Detect Image Edges"
-		 << "\n8-Enlarge Image"
-		 << "\n9-Shrink Image"
-		 << "\na-Mirror 1/2 Image"
-		 << "\nb-Shuffle Image"
-		 << "\nc-Blur Image"
-		 << "\nd-Crop Image"
-		 << "\ne-Skew Image Right"
-		 << "\nf-Skew Image Up"
-		 << "\ns-Save the image to a file"
-		 << "\n0-Exit\n";
+	loadImage(file_name + ".bmp");
 
-	switch (selection)
+	while (true)
 	{
-	case '1':
-		/* code */
-		break;
-	case '2':
-		/* code */
-		break;
-	case '3':
-		/* code */
-		break;
-	case '4':
-		/* code */
-		break;
-	case '5':
-		/* code */
-		break;
-	case '6':
-		/* code */
-		break;
-	case '7':
-		/* code */
-		break;
-	case '8':
-		/* code */
-		break;
-	case '9':
-		/* code */
-		break;
-	case 'a':
-		/* code */
-		break;
-	case 'b':
-		/* code */
-		break;
-	case 'c':
-		/* code */
-		break;
-	case 'd':
-		/* code */
-		break;
-	case 'e':
-		/* code */
-		break;
-	case 'f':
-		/* code */
-		break;
-	case 's':
-		/* code */
-		break;
-	default:
-		return (0);
-		break;
+		show_menu();
+		cin >> selection;
+
+		switch (selection)
+		{
+		case '1':
+			/* code */
+			break;
+		case '2':
+			/* code */
+			break;
+		case '3':
+			/* code */
+			break;
+		case '4':
+			/* code */
+			break;
+		case '5':
+			/* code */
+			break;
+		case '6':
+			/* code */
+			break;
+		case '7':
+			/* code */
+			break;
+		case '8':
+			/* code */
+			break;
+		case '9':
+			/* code */
+			break;
+		case 'a':
+			/* code */
+			break;
+		case 'b':
+			/* code */
+			break;
+		case 'c':
+			/* code */
+			break;
+		case 'd':
+			/* code */
+			break;
+		case 'e':
+			/* code */
+			break;
+		case 'f':
+			/* code */
+			break;
+		case 's':
+			saveImage();
+			break;
+		case '0':
+			return (0);
+		default:
+			cout << "Make sure you choose an available option.\n";
+			break;
+		}
 	}
 
 	return (0);
 }
 
+//==================================================================================================================================//
+
+void loadImage(string file_name)
+{
+	readGSBMP(file_name.c_str(), image);
+}
+
+void saveImage()
+{
+	char imageFileName[100];
+
+	// Get gray scale image target file name
+	cout << "Enter the target image file name: ";
+	cin >> imageFileName;
+
+	// Add to it .bmp extension and load image
+	strcat(imageFileName, ".bmp");
+	writeGSBMP(imageFileName, image);
+}
+
 /**
  * Ahlan - is a user welcome prompt.
  */
-void Ahlan(void)
+void Ahlan()
 {
 	cout << "Ahlan ya user ya habibi \1\n";
 	cout << "Please enter file name of the image to process: ";
+}
+
+/**
+ * show_menu - display a list of choices to choose a filter to apply.
+*/
+void show_menu()
+{
+cout << "Please select a filter to apply or 0 to exit:"
+	 << "\n1-Black & White Filter"
+	 << "\n2-Invert Filter"
+	 << "\n3-Merge Filter"
+	 << "\n4-Flip Image"
+	 << "\n5-Darken and Lighten Image"
+	 << "\n6-Rotate Image"
+	 << "\n7-Detect Image Edges"
+	 << "\n8-Enlarge Image"
+	 << "\n9-Shrink Image"
+	 << "\na-Mirror 1/2 Image"
+	 << "\nb-Shuffle Image"
+	 << "\nc-Blur Image"
+	 << "\nd-Crop Image"
+	 << "\ne-Skew Image Right"
+	 << "\nf-Skew Image Up"
+	 << "\ns-Save the image to a file"
+	 << "\n0-Exit\n";
 }
 
 /**
@@ -128,5 +171,6 @@ bool check_file_name(string file_name)
 		return (0);
 	}
 
+	cout << "Cannot open file: " << file_name <<endl;
 	return (1);
 }
